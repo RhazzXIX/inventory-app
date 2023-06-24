e0e61c36529265979609fc54d2709bf527701e2d
RhazzXIX <jm21tonio@gmail.com>
2023-06-24 17:29:16 +0800

    Add Item Schema/model

---
+++ models/item.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  category: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/shop/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
