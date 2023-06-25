b38ceb256a53211f843d93932b9e99772567fe0f
RhazzXIX <jm21tonio@gmail.com>
2023-06-25 23:51:35 +0800

    Add stocks routes

--- app.ts
+++ app.ts
@@ -8,7 +8,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const homeRouter = require('./routes/home')
+const homeRouter = require("./routes/home");
+const stocksRouter = require("./routes/stocks");
 
 mongoose.set("strictQuery", false);
 
@@ -36,7 +37,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use('/home', homeRouter);
+app.use("/home", homeRouter);
+app.use("/stocks", stocksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
